df492eba37ff4f173c70ed93bf7c533ff7729b08
Chris Miramontes <nyyankeeschris@gmail.com>
2018-06-06 19:28:12 -0400

    Module 1 completed

--- Module 1/src/Main.java
+++ Module 1/src/Main.java
@@ -4,8 +4,10 @@ import java.util.Map;
 
 public class Main {
 
+    // number of lines per ascii art letter
     private static final int letterStrHeight = 5;
 
+    // each letter as ascii art in a string with 5 lines
     private static final String A =
             "    A    \n" +
             "   A A   \n" +
@@ -97,19 +99,26 @@ public class Main {
             "  Y  \n" +
             "  Y  ";
 
+    // map to load art strings to
     private static Map<Character, String> asciiArt;
 
     public static void main(String[] args) {
 	    //MY JAVA USING INTELLIJ
+        // load strings into map
         loadAsciiArt();
+        // set string of what to draw on console
         final String myStr = "my java\nusing\nintellij";
+        // split string by new lines and draw line by line
         Arrays.stream(myStr.split("\n")).forEach(line -> {
+            // function to draw a string from ascii map
             printStringByLine(line);
+            // print new line to separate each string
             System.out.print('\n');
         });
     }
 
     private static void loadAsciiArt() {
+        // map each char letter to its corresponding string art
         asciiArt = new HashMap<>(13);
         asciiArt.put('a', A);
         asciiArt.put('e', E);
@@ -127,18 +136,26 @@ public class Main {
     }
 
     private static void printStringByLine(String str) {
+        // iterate over ascii art string lines which are all the same height as spec-ed above
         for(int i=0; i<letterStrHeight; i++) {
+            // take each char in string and print current line of outer for-loop
             for(char c : str.toCharArray()) {
+                // if char is recognized in map grab string, split by line, and output current line
+                // plus a space otherwise letters run into each other
                 if(asciiArt.containsKey(c)) {
                     System.out.print(asciiArt.get(c).split("\n")[i] + "  ");
                 }
+                // if a space just output a space like the original sentence
                 else if(c == ' ') {
                     System.out.print(" ");
                 }
+                // if given a string containing a letter not pre-rendered throw an exception
+                // assumption is we would not draw a string with a letter that wasn't pre-drawn
                 else {
                     throw new IllegalArgumentException("Input string contains character not drawn");
                 }
             }
+            // print new line before returning to separate lines like in the original sentence
             System.out.print("\n");
         }
     }
