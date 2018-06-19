19e38b9ce379d43ddc9c9ffc61c8ae7e208dbdfe
Chris Miramontes <nyyankeeschris@gmail.com>
2018-06-18 22:59:49 -0400

    module 3 intellij files

--- Module 3/src/GuessingGame.java
+++ Module 3/src/GuessingGame.java
@@ -7,6 +7,7 @@ public class GuessingGame {
         Scanner input = new Scanner( System.in );
         String again = "y";
 
+        // run as long as user responds y to continue prompt
         while(again.equals("y")) {
             System.out.println("What range would you like to guess: ");
             int range = input.nextInt();
@@ -15,11 +16,14 @@ public class GuessingGame {
             int answer = (int) (range * Math.random()) + 1;
             int guess = -1;
 
+            // run game so long as player has guesses left and hasn't
+            // guessed the right number
             while(guess != answer && guesses > 0) {
                 System.out.println("What is your guess: ");
                 guess = input.nextInt();
                 guesses--;
 
+                // only prompt user if guess is not correct
                 if(guess > answer) {
                     System.out.println("Too high");
                     System.out.println(String.format("%d guesses left!", guesses));
@@ -30,14 +34,8 @@ public class GuessingGame {
                 }
             }
 
-            if(guess == answer) {
-                System.out.println("You win! :D");
-            }
-            else {
-                System.out.println("You lose! D:");
-            }
-
-            System.out.println("Would you like to play again? (y/n): ");
+            String result = guess == answer ? "You win! :D" : "You lost! D:";
+            System.out.println(result + "\nWould you like to play again? (y/n): ");
             again = input.next();
         }
 
