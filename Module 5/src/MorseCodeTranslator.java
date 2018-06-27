20483e41f64a3be815a2e8e27e3bd45a9efd0775
Chris Miramontes <nyyankeeschris@gmail.com>
2018-06-26 20:36:58 -0400

    added comments to module 5

--- Module 5/src/MorseCodeTranslator.java
+++ Module 5/src/MorseCodeTranslator.java
@@ -25,75 +25,138 @@ public class MorseCodeTranslator
         String sentence;
         int choice = 0;
 
-        while(!isValidChoice(choice)) {
+        // loop until user enters a valid input choice
+        while(!isValidChoice(choice))
+        {
             System.out.print("Enter 1 for English or 2 for Morse input: ");
             choice = input.nextInt();
 
-            if (isValidChoice(choice)) {
+            if (isValidChoice(choice))
+            {
                 System.out.println("Please enter your sentence: ");
                 input.nextLine();
-                while(!(sentence = input.nextLine()).isEmpty()) {
+                while(!(sentence = input.nextLine()).isEmpty())
+                {
                     String translation = choice == 1 ?
                             englishToMorse(sentence) : morseToEnglish(sentence);
                     System.out.println(translation);
                 }
-            } else {
+            } else
+                {
                 System.out.println("Please enter a valid input");
             }
         }
 
     }
 
+    /***
+     * Determines if input was a valid choice i.e 1->English 2->Morse
+     *
+     * @param choice value of user input
+     *
+     * @return boolean if valid or not
+     */
     private static boolean isValidChoice(int choice)
     {
         return choice == 1 || choice == 2;
     }
 
+    /***
+     * Translates ascii letters to morse code seperated by spaces
+     * and words seperated by the pipe symbol -> |
+     *
+     * @param english input sentence from the user
+     *
+     * @return String of equivalent input in morse code
+     */
     private static String englishToMorse(String english)
     {
         StringBuilder translation = new StringBuilder();
+        // ascii code of capital letters starts at 65
+        // offset by 10 because numbers come first in alphabet
+        // defined above
         final int letterOffset = ((int) 'A') - 10;
+
+        // split sentence by space delimeter
         String[] words = english.split(" ");
+
+        // loop over each word and translate each letter
         for(String word : words) {
             String morseStr;
-            for (int i = 0; i < word.length(); i++) {
+            // loop over individual letters
+            for (int i = 0; i < word.length(); i++)
+            {
                 char letter = word.charAt(i);
-                if (Character.isAlphabetic(letter)) {
+                // if its a letter grab the ascii code and subtract the offset
+                if (Character.isAlphabetic(letter))
+                {
                     letter = Character.toUpperCase(letter);
                     morseStr = morse[(int) letter - letterOffset];
                     translation.append(morseStr).append(" ");
-                } else if (Character.isDigit(letter)) {
+                }
+                // if its a number just use its value as array index
+                else if (Character.isDigit(letter))
+                {
                     morseStr = morse[(int) letter];
                     translation.append(morseStr).append(" ");
                 }
             }
+            // separate each word with a pipe symbol
             translation.append("| ");
         }
         return translation.toString();
     }
 
+    /***
+     * Translates a morse string with words separated by pipes into
+     * english letters.
+     *
+     * @param morse String with letters separated by spaces and words
+     *              separated by pipe
+     *
+     * @return String of equivalent english letters and numbers
+     */
     private static String morseToEnglish(String morse)
     {
         StringBuilder translation = new StringBuilder();
+
+        // split string by words separated by pipes
         String[] words = morse.split("\\|");
-        for(String word : words) {
+        for(String word : words)
+        {
+            // split each word into individual letters delimited by a space
             String[] letters = word.split(" ");
-            for(String morseLetter : letters) {
+            for(String morseLetter : letters)
+            {
+                // use function to return index of equivalent morse code
                 int arrayIndex = getMorseIndex(morseLetter);
-                if(arrayIndex >= 0) {
+                // if -1 it didn't recognize code so don't index on alphabet
+                if(arrayIndex >= 0)
+                {
                     translation.append(alphabet[arrayIndex]);
                 }
             }
+            // separate each word with a space
             translation.append(" ");
         }
 
         return translation.toString();
     }
 
+    /***
+     * Iterates over morse array and returns index of matching string or
+     * -1 if it is not found
+     *
+     * @param morseCode String of morse code
+     *
+     * @return int of index in alphabet of equivalent letter or -1 if not found
+     */
     private static int getMorseIndex(String morseCode)
     {
-        for(int i=0; i<morse.length; i++) {
-            if(morseCode.equals(morse[i])) {
+        for(int i=0; i<morse.length; i++)
+        {
+            if(morseCode.equals(morse[i]))
+            {
                 return i;
             }
         }
